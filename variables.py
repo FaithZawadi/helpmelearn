d99fbfd4ae1bf70f1c252fe5a6d0e5b86885f11e
FaithZawadi <79697379+FaithZawadi@users.noreply.github.com>
2023-08-03 21:25:27 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+x=10 #int variable
+y=54.0 #float variable
+z="haha" #string variable
+print(x)
+print(y)
+print(z)
+#declared string ,int and float
+#different naming protocols for variables
+myFullName="Faith Zawadi" #camel case
+MyFullName="Eric Dwein" #pascal case
+my_full_name="Gem Ginnie"#snake case
+print(my_full_name)
+print(myFullName)
+print(MyFullName)
+#first variables represent things that are dear to me
+myBaby="pearl"
+myJob="Software engineering"
+my_favorite_number=7
+print(myBaby)
+print(myJob)
+print(my_favorite_number)
+#variables that show my reasons for joining this course
+reason_1="sharpening my python skills"
+reason_2="extending my tech community"
+reason_3="I want to use python as the main language for development"
+print(reason_1)
+print(reason_2)
+print(reason_3)
+
+
+
