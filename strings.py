ba2bd60fce883f781898a17c886bef31e53153da
FaithZawadi <79697379+FaithZawadi@users.noreply.github.com>
2023-08-03 14:18:30 +0300

    format strings

--- strings.py
+++ strings.py
@@ -1,4 +1,18 @@
 print("hello world")
-txt="zawadi is incredible"
+txt = "zawadi is incredible"  # assign string a variable
 print(txt)
-print()
+a = """Helloshfggmmdnk fghfjasak d      ashhjkdhasjkdhjfsdjhgfhkhakkfhkfdjhfgjhajkgjhhfjshjdghf"""
+print(txt[2:5])  # slicing strings
+print(txt[:5])
+print("zawadi" in txt)
+print(len(txt))
+print(txt.upper())
+print(txt.lower())
+print(txt.replace("z", "x"))
+print(txt.split(","))
+x = "child"
+y = "birth"
+z = x + " " + y
+age = 26
+statement = "my boyfriens is {} years old"
+print(txt.format(age))
