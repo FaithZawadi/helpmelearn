88b9ba4b72367ed730699f10d0149d37a53160ea
FaithZawadi <79697379+FaithZawadi@users.noreply.github.com>
2023-08-04 20:52:04 +0300

    about me

--- about me.py
+++ about me.py
@@ -1,6 +1,9 @@
 print("I am a software developer with two years experience in front end development")
 print("while moving on with my carrier as an engineer i allso want to inspire more girls to join us")
 print("always be ambitious")
+print("i cant stop")
+print("Tech loves me and am just using it")
+print("Python isnt a snake apparently")
 name="Zawadi"
 print(name)
 text="life is a gift cherish it and use it"
